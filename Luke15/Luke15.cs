aab2020b65a8168bd43a5a94de395b88f1b4543b
Terje Wiesener <wiesener@gmail.com>
2020-12-16 00:14:07 +0100

    Cleanup Luke15

--- Luke15/Luke15.cs
+++ Luke15/Luke15.cs
@@ -12,13 +12,21 @@ namespace Luke15
             int sum = 0;
             Performance.TimeRun("LINQ solution", () =>
             {
-                var combinations = TextFile.ReadStringList("riddles.txt");
-                var dictionary = TextFile.ReadStringSet("wordlist.txt");
-                var pairs = combinations.Select(c => c.Split(", ")).Select(arr => (arr[0], arr[1])).ToArray();
-                sum = dictionary.AsParallel().Where(word => pairs.Any(p => dictionary.Contains($"{p.Item1}{word}") && dictionary.Contains($"{word}{p.Item2}"))).Sum(w => w.Length);
+                sum = Solve();
             }, 10, 1, 3);
 
             Console.WriteLine(sum);
+        }
+
+        static int Solve()
+        {
+            var combinations = TextFile.ReadStringList("riddles.txt");
+            var dictionary = TextFile.ReadStringSet("wordlist.txt");
+            var pairs = combinations.Select(c => c.Split(", ")).Select(arr => (arr[0], arr[1])).ToArray();
+            return dictionary
+                .AsParallel()
+                .Where(word => pairs.Any(p => dictionary.Contains($"{p.Item1}{word}") && dictionary.Contains($"{word}{p.Item2}")))
+                .Sum(w => w.Length);
         }
     }
 }
